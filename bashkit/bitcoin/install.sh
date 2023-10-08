3799e8c08171fae888ba324c5c348d2c6d66cb94
ajrlewis <git@ajrlewis.com>
2023-10-08 17:52:48 +0100

    Added the required dependencies for building the bitcoin-qt frontend, including the qt5core library.

--- bashkit/bitcoin/install.sh
+++ bashkit/bitcoin/install.sh
@@ -54,7 +54,7 @@ git checkout "$LATEST_VERSION"
 # Install the dependencies needed to build Bitcoin
 echo "Installing dependencies..."
 apt-get update && \
-apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev
+apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
 
 # Configure the build process
 echo "Configuring build process..."
